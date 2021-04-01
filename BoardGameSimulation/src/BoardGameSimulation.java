b0650deace9ad72c229a2a4b7484b5a5273a594d
rlhughes1987 <rlhughes1987@gmail.com>
2021-04-01 15:02:04 +0100

    Update BoardGameSimulation.java

--- BoardGameSimulation/src/BoardGameSimulation.java
+++ BoardGameSimulation/src/BoardGameSimulation.java
@@ -1,42 +1,6 @@
 
 public class BoardGameSimulation {
 	
-	// ---- MORE SOCIAL INTERACTION GAME ------
-	//game could simulate a newbie entering a prison.
-	//the newbie has an objective to forfill.
-	//other players may or may not know what the player is in for, and also may or may not know their objective.
-	//can exchange cards with each other to influence their opinion.
-	//one player is an officer and if catches anyone doing anything wins alone.
-	
-	
-	// ---- MORE BOARD GAME GAME -----
-	//each player wants to escape
-	//players have a protagnoist and can build a collection of pawns - worker plaement
-	// protagonistc is respect amongst inmates and or officers.
-	// events trigger based on location deck
-	// simulation ROUTINE by having set days for the library perhaps. 
-	// Calendar might be different at the start to simulate different polices of a prison
-	// moving a protagonist means you draw from a deck of social interactions.. dave say shey. some are sinister.
-	// 
-	
-	// -- God father type ----
-	// Relationship / Interactions.
-	// God Father / Snitch / Right-hand man / Son / Wife / Girlfriend
-	//     Rock 
-	// Paper Scissors
-	
-	//Destructive Associations
-	//    Father
-	//  Son   Snitch
-	
-	//   GF  Wife
-	//    RH man
-	
-	//Constructive Associations
-	
-	//    RH Man
-	// GF   Father
-	
 	
 	private Setup setup;
 	
